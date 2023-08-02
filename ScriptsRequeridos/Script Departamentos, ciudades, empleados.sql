b86ed6c0e31de4978c153d41ccfd3439e0040f02
yojann96 <yojann96@hotmail.com>
2023-08-02 09:55:05 -0500

    sctipt para crear registros bases de estados, ciudades

---
+++ ScriptsRequeridos/Script Departamentos, ciudades, empleados.sql
@@ -0,0 +1,9 @@
+INSERT INTO departamentos VALUES (1,'Cundimarca','',now(),now());
+INSERT INTO departamentos VALUES (2,'Santander','',now(),now());
+
+INSERT INTO ciudades VALUES (1,'Bogotá',1,'',now(),now());
+INSERT INTO ciudades VALUES (2,'Soacha',1,'',now(),now());
+INSERT INTO ciudades VALUES (3,'Bucaramanga',2,'',now(),now());
+INSERT INTO ciudades VALUES (4,'San Gil',2,'',now(),now());
+
+INSERT INTO empleados VALUES (1,'Prueba','Usuario','123','Falsa123','313920',1,1,'',now(),now());
